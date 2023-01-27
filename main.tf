bf1f846ba2e5c440760665004b24305a5f133410
chuleh <lcasarettom@gmail.com>
2023-01-27 13:17:26 -0300

    WIP: main.tf | README

---
+++ main.tf
@@ -0,0 +1,14 @@
+module "vpc" {
+  source = "git@github.com:chuleh/tf-aws-vpc-module.git"
+
+  name = "remotews"
+  cidr = "192.168.0.0/16"
+  public_subnets = ["192.168.1.0/24"]
+  azs = ["us-east-1a"]
+  map_public_ip_on_launch = true
+}
+
+module "ec2" {
+  source = "git@github.com:chuleh/tf-aws-ec2-module.git"
+  
+}
