bb0c3f50cfdb27fbc27d21dc0064f1754a004f5b
chuleh <lcasarettom@gmail.com>
2023-01-28 01:05:24 -0300

    WIP: fix ci | fmt

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "git@github.com:chuleh/tf-aws-vpc-module.git"
+  source = "github.com/chuleh/tf-aws-vpc-module.git"
 
   name                    = "remotews"
   cidr                    = "192.168.0.0/16"
@@ -9,7 +9,8 @@ module "vpc" {
 }
 
 module "ec2" {
-  source         = "git@github.com:chuleh/tf-aws-ec2-module.git"
+  source = "github.com/chuleh/tf-aws-ec2-module.git"
+
   ami            = "ami-0ff8a91507f77f867"
   public_subnets = [module.vpc.public_subnets]
   create_eip     = true
